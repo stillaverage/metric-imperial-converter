3a7f69671bc61180b41c742c4bb0ad88255412d5
Sean Hamilton <stillaverage@gmail.com>
2024-07-05 17:28:44 -0700

    feat: convert handler logic

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,32 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get((req, res, done) => {
+    const input = req.query.input
+    console.log(input)
+    const initNum = convertHandler.getNum(input)
+    console.log(initNum)
+    const initUnit = convertHandler.getUnit(input)
+    console.log(initUnit)
+    const returnUnit = convertHandler.getReturnUnit(initUnit)
+    console.log(returnUnit)
+    const returnNum = convertHandler.convert(initNum, initUnit)
+    console.log(returnNum)
+    let string;
+    if (isNaN(initNum) && initUnit === 'invalid') {
+      string = 'invalid number and unit'
+    } else if (isNaN(initNum)) {
+      string = 'invalid number'
+    } else if (initUnit === 'invalid') {
+      string = 'invalid unit'
+    } else {
+      string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+    }
+    console.log(string)
+    if (!string.includes('invalid')) {
+      res.json({ initNum, initUnit, returnNum, returnUnit, string})
+    } else {
+      res.json({ string }.string)
+    }
+  })
 };
